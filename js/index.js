45f0e50979155941bc306fdbe4e1e5e566227662
mdafsinur <afsin.sayem1@gmail.com>
2020-11-04 08:11:52 +0600

    Alhamdulillah v-0.1 | friend users.

---
+++ js/index.js
@@ -0,0 +1,72 @@
+window.addEventListener("load", () => {
+  document.querySelector("#app").style.display = "block";
+
+  new Vue({
+    el: "#app",
+    data: {
+      userName: "",
+      conditions: {
+        hideWhiteDivData: false,
+        fromLeft: false,
+        fromRight: false,
+      },
+      userForm: {
+        idGmail: "",
+        idText: "",
+        idPassword: "",
+      },
+    },
+    methods: {
+      viwerGoSubmit(e) {
+        if (e != "") {
+          var strln1 = e.length;
+          if (strln1 > 10) {
+            var slc1 = e.slice(strln1 - 10, strln1);
+            var mtc1 = "@gmail.com";
+            var include1 = slc1.includes(mtc1);
+            console.log(include1, strln1);
+          } else {
+            var alert_2 = "Please enter a valid gmail below!";
+            alert(alert_2);
+          }
+        } else {
+          var alert_1 = "Please fill out the form below!";
+          alert(alert_1);
+        }
+      },
+      userGoSubmited(e1, e2) {
+        if (e1 != "" && e2 != "") {
+          console.log(e1, e2);
+        } else {
+          var alert_1 = "Please fill out the form below!";
+          alert(alert_1);
+        }
+      },
+      hideWhiteDiv(e) {
+        if (e == 0 || e == 1) {
+          this.conditions.hideWhiteDivData = true;
+          if (e == 0) {
+            this.conditions.fromLeft = true;
+          } else if (e == 1) {
+            this.conditions.fromRight = true;
+          }
+        }
+      },
+      changeTitleAndNavUsername() {
+        db.collection("oneUser")
+          .get()
+          .then((data) => {
+            data.docs.forEach((doc) => {
+              this.userName = doc.data().name;
+              document.title = this.userName;
+              document.querySelector(".loadingContainer").style.display =
+                "none";
+            });
+          });
+      },
+    },
+    mounted() {
+      this.changeTitleAndNavUsername();
+    },
+  });
+});
